7a59af106fb79b23daf6c85147f6258e3c1a358c
unknown <cls>
2026-02-19 09:33:32 +0530

    change in axiox

--- lib/axios.ts
+++ lib/axios.ts
@@ -11,6 +11,10 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export default axiosInstance;
+
